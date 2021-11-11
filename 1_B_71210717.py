ae7011683374dbe20c9633a50d1481936c4bc73b
YedizaReinSahupala <93330433+YedizaReinSahupala@users.noreply.github.com>
2021-11-12 00:52:16 +0900

    Add files via upload

---
+++ 1_B_71210717.py
@@ -0,0 +1,14 @@
+Nama= input("Nama: ")
+nama1= Nama.split()[-1]
+nama2= Nama.split(" ", 1)[0]
+Ttl = input("Tempat tanggal lahir: ")
+Ttl2= Ttl.split(" ", 1)
+#Inyint Milzyrin
+#Uzulun Umurusutu Tyuruk
+print("Haloo!", nama1 + ",", nama2)
+
+print("Anda lahir di", Ttl2[0] + " pada tanggal", Ttl2[1] )
+
+
+
+
