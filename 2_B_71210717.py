2e6085122b738f0d863937da8edcb79453222daa
YedizaReinSahupala <93330433+YedizaReinSahupala@users.noreply.github.com>
2021-11-12 00:53:21 +0900

    Add files via upload

---
+++ 2_B_71210717.py
@@ -0,0 +1,9 @@
+r= float(input("Masukan jari-jari alas tabung: "))
+t= float(input("Masukan tinggi tabung: "))
+Pi= 3.14
+Luas_permukaan= (2 * Pi * r * (r + t))
+#Volume= (Pi * r**2 * t)
+
+print("Luas permukaan tabung adalah ", Luas_permukaan)
+#print("Volume tabung adalah ", Volume)
+#Gunakan kode yang ada di komen agar mendapat nilai yang sama dengan yang ada di UG
