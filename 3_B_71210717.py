590b7716c17a1cf1fcb1f45f5404f5aa107b144e
YedizaReinSahupala <93330433+YedizaReinSahupala@users.noreply.github.com>
2021-11-12 00:56:54 +0900

    Add files via upload

---
+++ 3_B_71210717.py
@@ -0,0 +1,9 @@
+nilai1= int(input("Nilai 1: "))
+nilai2= int(input("Nilai 2: "))
+nilai3= int(input("Nilai 3: "))
+
+avarage= (nilai1 + nilai2 + nilai3)/3
+
+avarage_string= "{} + {} + {}" .format(nilai1, nilai2, nilai3)
+
+print("Rata-Rata dari ", avarage_string, "adalah ", avarage)
